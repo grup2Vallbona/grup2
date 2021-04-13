7a70c415b69a4b61c22af244510b6e708fcdbd79
Albert Martínez <albertmf0607@gmail.com>
2021-04-13 20:28:59 +0200

    Inici server

---
+++ server/app/Models/Gent_ensenyada.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gent_ensenyada extends Model
+{
+    use HasFactory;
+}
---
+++ server/app/Models/Pertanyen_professor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pertanyen_professor extends Model
+{
+    use HasFactory;
+}
