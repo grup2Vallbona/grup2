50d549f228ef4ce8540eb3b208a2fce8c9b5a97f
Nil <nilrocabayera2@gmail.com>
2021-04-27 14:59:02 +0200

    conflictes principal

--- client/src/pages/principal/principal.ts
+++ client/src/pages/principal/principal.ts
@@ -3,17 +3,10 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { Login } from "../login/login";
 import { Register } from "../register/register";
 import { DadesProductesService } from "../../services/dades-productes.service";
-<<<<<<< HEAD
 import { Eventos } from "../eventos/eventos";
 import { Foro } from "../foro/foro";
 import { Valoraciones } from "../valoraciones/valoraciones";
 import { Novedades } from "../novedades/novedades";
-=======
-import { Eventos } from '../eventos/eventos';
-import { Foro } from '../foro/foro';
-import { Valoraciones } from '../valoraciones/valoraciones';
-import { Novedades } from '../novedades/novedades';
->>>>>>> 27a3fbc836a32453899ffc3e06429251ce1e96da
 import { Storage } from '@ionic/storage';
 
 /**
@@ -28,18 +21,12 @@ import { Storage } from '@ionic/storage';
   templateUrl: "principal.html",
 })
 export class Principal {
-<<<<<<< HEAD
   emaail: string;
-=======
-
-  email:string;
->>>>>>> 27a3fbc836a32453899ffc3e06429251ce1e96da
   tab1Root = Novedades;
   tab2Root = Foro;
   tab3Root = Valoraciones;
   tab4Root = Eventos;
 
-<<<<<<< HEAD
   constructor(
     private dades: DadesProductesService,
     public navParams: NavParams,
@@ -48,14 +35,6 @@ export class Principal {
     this.emaail = navParams.get("email");
     storage.set('email', this.emaail);
     
-=======
-  constructor(private dades: DadesProductesService,public storage: Storage,private navParams:NavParams) {
-    this.setSession(navParams.get("email"));
-  }
-   setSession(value){
-    return this.storage.set(`setting:email`,value);
-  }
->>>>>>> 27a3fbc836a32453899ffc3e06429251ce1e96da
   
   }
 }
